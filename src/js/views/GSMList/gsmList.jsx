fe290af13e65735ad3dcda00255b4209677a761b
Brandon <b.nutty@gmail.com>
2020-01-15 06:03:41 -0800

    gsm list and adjusted size of cards

--- src/js/views/GSMList/gsmList.jsx
+++ src/js/views/GSMList/gsmList.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class ESNList extends Component {
+export default class GSMList extends Component {
     render() {
         const { requests, onlyMine } = this.props;
 
@@ -9,29 +9,29 @@ export default class ESNList extends Component {
         if(onlyMine === true) {
             content = 
                 <div className="card-content">
-                    <span className="card-title"><b>ESN List</b></span>
+                    <span className="card-title"><b>GSM List</b></span>
                     <hr/>
-                    <div className="card-content" id="esnList">
+                    <div className="card-content" id="gsmList">
                         {!!requests && requests.filter(request => request.processor === 'Brandon').map(request =>
-                            <p className={request.color + '-text'} key={request.requestId}><b>{request.esn}</b></p>
+                            <p className={request.color + '-text'} key={request.requestId}><b>{request.gsm}</b></p>
                         )}
                     </div>
                 </div>
         } else {
             content = 
                 <div className="card-content">
-                    <span className="card-title"><b>ESN List</b></span>
+                    <span className="card-title"><b>GSM List</b></span>
                     <hr/>
-                    <div className="card-content" id="esnList">
+                    <div className="card-content" id="gsmList">
                         {!!requests && requests.filter(request => request.hidden === false).map(request =>
-                            <p className={request.color + '-text'} key={request.requestId}><b>{request.esn}</b></p>
+                            <p className={request.color + '-text'} key={request.requestId}><b>{request.gsm}</b></p>
                         )}
                     </div>
                 </div>
         }
 
         return (
-            <div className="col s12 m2 l2">
+            <div className="col s12 m3 l3">
                 <div className="card grey darken-3 white-text center">
                     {content}
                 </div>
